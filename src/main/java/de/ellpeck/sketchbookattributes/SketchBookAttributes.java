4d3b4400883eecaf9cb7570f0d21d0a10d3dfe0f
Ellpeck <me@ellpeck.de>
2022-03-02 20:15:32 +0100

    changed some defaults

--- src/main/java/de/ellpeck/sketchbookattributes/SketchBookAttributes.java
+++ src/main/java/de/ellpeck/sketchbookattributes/SketchBookAttributes.java
@@ -3,8 +3,6 @@ package de.ellpeck.sketchbookattributes;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -16,8 +14,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class SketchBookAttributes {
 
     public static final String ID = "sketchbookattributes";
-    @CapabilityInject(AttributeData.class)
-    public static final Capability<AttributeData> ATTRIBUTE_DATA_CAPABILITY = null;
 
     public static ConfigValue<Double> xpNeededMultiplier;
     public static ConfigValue<Double> healthRegenPerLevel;
@@ -40,7 +36,7 @@ public class SketchBookAttributes {
                 .define("xp_needed_multiplier", 1D);
         healthRegenPerLevel = configBuilder
                 .comment("The amount of health regen that an additional level of the ability adds per second")
-                .define("health_regen_per_level", 0.02);
+                .define("health_regen_per_level", 0.04);
         manaRegenPerLevel = configBuilder
                 .comment("The amount of mana regen that an additional level of the ability adds per second")
                 .define("mana_regen_per_level", 0.02);
@@ -61,7 +57,7 @@ public class SketchBookAttributes {
                 .define("ranged_speed_per_level", 0.1);
         movementSpeedPerLevel = configBuilder
                 .comment("The amount that movement speed is increased for each addditional level of the ability")
-                .define("movement_speed_per_level", 0.1);
+                .define("movement_speed_per_level", 0.0025);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, configBuilder.build());
     }
 
