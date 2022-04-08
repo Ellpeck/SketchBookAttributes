d59480edafb936ba7fc96134443c60d897fd70f5
Ellpeck <me@ellpeck.de>
2022-04-08 23:13:52 +0200

    made the goddamn greatsword item work lmao

--- src/main/java/de/ellpeck/sketchbookattributes/items/GreatswordItem.java
+++ src/main/java/de/ellpeck/sketchbookattributes/items/GreatswordItem.java
@@ -1,12 +1,19 @@
 package de.ellpeck.sketchbookattributes.items;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 
 public class GreatswordItem extends SwordItem {
 
     public GreatswordItem(IItemTier tier, int damage, float speed, int durability) {
         super(tier, damage, speed, new Properties().tab(ItemGroup.TAB_COMBAT).durability(durability));
+    }
+
+    @Override
+    public boolean canDisableShield(ItemStack stack, ItemStack shield, LivingEntity entity, LivingEntity attacker) {
+        return true;
     }
 }
